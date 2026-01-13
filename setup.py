250de1d8495a33a950f64d2e5d6902b05daff662
KHAJA_MUBASHIR_ARSALAN <kmarsalan124@gmail.com>
2026-01-13 17:11:52 +0530

    latest version 0.1.1

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    description = f.read()
+
 setup(
     name="myeda",
-    version="0.1.0",
+    version="0.1.1",
     description="Lightweight Exploratory Data Analysis library",
-    author="Your Name",
+    author="Khaja Mubashir Arsalan",
     packages=find_packages(),
     install_requires=[
         "pandas",
         "matplotlib",
     ],
     python_requires=">=3.9",
+
+    long_description = description,
+    long_description_content_type="text/markdown",
 )
+
+
+
+
